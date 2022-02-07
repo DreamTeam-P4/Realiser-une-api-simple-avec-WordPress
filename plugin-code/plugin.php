28952e63e05db1f1bf23d5aa8cde58dc9588370e
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-02-07 22:31:05 +0000

    Le code source du plugin permettant de generer des champs personalisés

---
+++ plugin-code/plugin.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Plugin Name:DreamTeam
+ * Description:"plugin permettant de générer les CPT et Champs personnalisés sous wordpress"
+ * Author:Epsilon
+ */
+
+acf_add_local_field_group([
+    'key' => 'Formation Simplon',
+    'title' => 'Formation Simplon',
+    'fields' => [
+        [
+            'key' => 'field_title',
+            'label' => 'Title',
+            'name' => 'title',
+            'type' => 'text',
+        ],
+        [
+            'key' => 'field_description',
+            'label' => 'Description',
+            'name' => 'description',
+            'type' => 'textarea',
+        ],
+        [
+            'key' => 'field_image',
+            'label' => 'Image',
+            'name' => 'image',
+            'type' => 'image',
+        ],
+        [
+            'key' => 'field_link',
+            'label' => 'Link',
+            'name' => 'image',
+            'type' => 'link',
+        ]
+
+    ],
+    'location' => [
+        [
+            [
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'formations',
+            ],
+        ],
+    ],
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+]);
